46b7eba73dd6da97fb238f2f2d50a8931c38a10f
rick.maniquiz <rafaelnico.maniquiz@gcash.com>
2025-06-30 16:13:58 +0800

    Final Submission Lab 2: Membership Calculator

---
+++ README.md
@@ -0,0 +1,6 @@
+# LAB 1: Celcius to Farenheit Converter
+
+### Cadet Name: Rafael Nico T. Maniquiz
+### Project Name: FECP-Java-Session1-ControlStructures
+### Output Screenshot: 
+<img src="https://github.com/rick-maniquiz/FECP-Java-Session1-ControlStructures/blob/6ea19b7df1692cf2d0a4062dce879b1bf2d58b8e/screenshot.png"/>
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,53 @@ import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String membershipStatus = scanner.nextLine();
+        float membershipPrice = 0.0f;
+        switch (membershipStatus.toLowerCase()){
+            case "regular":{
+                System.out.print("Enter age: ");
+                int age = scanner.nextInt();
+                if (age < 18){
+                    membershipPrice = 50.0f;
+                } else if (age >= 18 && age <= 64) {
+                    membershipPrice = 100.0f;
+                } else {
+                    membershipPrice = 75.0f;
+                }
+                break;
+            }
+            case "vip":{
+                System.out.print("Enter age: ");
+                int age = scanner.nextInt();
+                if (age < 18){
+                    membershipPrice = 75.0f;
+                } else if (age >= 18 && age <= 64) {
+                    membershipPrice = 150.0f;
+                } else {
+                    membershipPrice = 112.5f;
+                }
+                break;
+            }
+            case "premium":{
+                System.out.print("Enter age: ");
+                int age = scanner.nextInt();
+                if (age < 18){
+                    membershipPrice = 100.0f;
+                } else if (age >= 18 && age <= 64) {
+                    membershipPrice = 200.0f;
+                } else {
+                    membershipPrice = 150.0f;
+                }
+                break;
+            }
+            default:
+                System.out.print("Invalid membership status entered.");
+                return;
+        }
+        System.out.print("Price: $" + String.format("%.2f", membershipPrice));
     }
 }
